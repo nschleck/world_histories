444703a56382423fb9ace2f15e6bcc3f31e2b7f4
Nick Schleck <38449492+nschleck@users.noreply.github.com>
2023-11-15 22:52:41 -0600

    Update main.py
    bundled UI objects into UI TopBar class

--- main.py
+++ main.py
@@ -18,58 +18,52 @@ clock = pyg.time.Clock()
 #Create World Events List
 testEvents()
 
-
-
 #CONSTRUCT UI TOPBAR
-#top_bar = pyg.Surface((SCREEN_WIDTH,100))
-top_bar_UI = pygame_gui.elements.UIPanel(relative_rect=pyg.Rect((0,0),(SCREEN_WIDTH,100)),
+class UI_TopBar:
+    def __init__(self) -> None:
+        self.panel = pygame_gui.elements.UIPanel(relative_rect=pyg.Rect((0,0),(SCREEN_WIDTH,100)),
                                          manager=manager)
-row = [5,40]
-col = [20,200,400,590,780,970]
+        
+        self.row = [5,40]
+        self.col = [20,200,400,590,780,970]
 
-UIdate_start = pygame_gui.elements.UITextEntryLine(relative_rect=pyg.Rect((col[0],row[1]),(150,30)),
+        self.date_start_entry = pygame_gui.elements.UITextEntryLine(relative_rect=pyg.Rect((self.col[0],self.row[1]),(150,30)),
                                                 initial_text="0",
                                                 manager=manager,
-                                                container=top_bar_UI)
-UIdate_end = pygame_gui.elements.UITextEntryLine(relative_rect=pyg.Rect((col[1],row[1]),(150,30)),
+                                                container=self.panel)
+        self.date_end_entry = pygame_gui.elements.UITextEntryLine(relative_rect=pyg.Rect((self.col[1],self.row[1]),(150,30)),
                                               initial_text="2000 CE",
                                               manager=manager,
-                                              container=top_bar_UI)
-UIdate_start_label = pygame_gui.elements.UILabel(relative_rect=pyg.Rect((col[0],row[0]),(150,50)),
+                                              container=self.panel)
+        self.label_date_start = pygame_gui.elements.UILabel(relative_rect=pyg.Rect((self.col[0],self.row[0]),(150,50)),
                                                text='Start Date',
                                                manager=manager,
-                                               container=top_bar_UI)
-UIdate_start_label = pygame_gui.elements.UILabel(relative_rect=pyg.Rect((col[1],row[0]),(150,50)),
+                                               container=self.panel)
+        self.label_date_end = pygame_gui.elements.UILabel(relative_rect=pyg.Rect((self.col[1],self.row[0]),(150,50)),
                                                text='End Date',
                                                manager=manager,
-                                               container=top_bar_UI)
-'''date_header = pygame_gui.elements.UILabel(relative_rect=pyg.Rect(((col2+col1)/2,-12),(150,50)),
-                                               text='Date Range',
-                                               manager=manager,
-                                               container=top_bar_UI)'''
+                                               container=self.panel)
 
-tag_list_type = pygame_gui.elements.UISelectionList(relative_rect=pyg.Rect((col[2],row[0]),(180,70)),
+        self.type_selector = pygame_gui.elements.UISelectionList(relative_rect=pyg.Rect((self.col[2],self.row[0]),(180,70)),
                                                 item_list=['All'] + tagDict["type"],
                                                 allow_multi_select=True,
                                                 manager=manager,
-                                                container=top_bar_UI)
-tag_list_era = pygame_gui.elements.UISelectionList(relative_rect=pyg.Rect((col[3],row[0]),(180,70)),
+                                                container=self.panel)
+        self.era_selector = pygame_gui.elements.UISelectionList(relative_rect=pyg.Rect((self.col[3],self.row[0]),(180,70)),
                                                 item_list=['All'] + tagDict["era"],
                                                 allow_multi_select=True,
                                                 manager=manager,
-                                                container=top_bar_UI)
-tag_list_culture = pygame_gui.elements.UISelectionList(relative_rect=pyg.Rect((col[4],row[0]),(180,70)),
+                                                container=self.panel)
+        self.culture_selector = pygame_gui.elements.UISelectionList(relative_rect=pyg.Rect((self.col[4],self.row[0]),(180,70)),
                                                 item_list=['All'] + tagDict["culture"],
                                                 allow_multi_select=True,
                                                 manager=manager,
-                                                container=top_bar_UI)
-tag_list_region = pygame_gui.elements.UISelectionList(relative_rect=pyg.Rect((col[5],row[0]),(180,70)),
+                                                container=self.panel)
+        self.region_selector = pygame_gui.elements.UISelectionList(relative_rect=pyg.Rect((self.col[5],self.row[0]),(180,70)),
                                                 item_list=['All'] + tagDict["region"],
                                                 allow_multi_select=True,
                                                 manager=manager,
-                                                container=top_bar_UI)
-
-
+                                                container=self.panel)
 
 test_button = pygame_gui.elements.UIButton(relative_rect=pyg.Rect((0,0),(150,50)),
                                            text="print dates",
@@ -77,31 +71,24 @@ test_button = pygame_gui.elements.UIButton(relative_rect=pyg.Rect((0,0),(150,50)
                                            manager=manager,
                                            anchors={'center': 'center'})
 
-for worldEvent in worldEvents:
-    print(worldEvent.typeTag) 
-
-def getSelectedEvents():
+def getEventsByTag(ui_topbar):
     selected_events = []
     for worldEvent in worldEvents:
-        common_tags = list(set(worldEvent.typeTag).intersection(tag_list_type.get_multi_selection()))
-        if len(common_tags) > 0 or "All" in tag_list_type.get_multi_selection():
+        common_tags = list(set(worldEvent.typeTag).intersection(ui_topbar.type_selector.get_multi_selection()))
+        if len(common_tags) > 0 or "All" in ui_topbar.type_selector.get_multi_selection():
             selected_events.append(worldEvent)
     return selected_events
+
+#Create UI Elements
+UI_topbar = UI_TopBar()
 
 
 #MAIN APP LOOP
 while True:
     dt = clock.tick(60)/1000.0   
     
-    display_events = getSelectedEvents()
+    display_events = getEventsByTag(UI_topbar)
 
-    '''try:
-        date_start = dateFormatToInt(UIdate_start.get_text())
-        date_end = dateFormatToInt(UIdate_end.get_text())
-    except:
-        date_start = "N/A"
-        date_end = "N/A"'''
-    
     #EVENT LOOP
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
